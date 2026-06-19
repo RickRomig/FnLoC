5f66abece3825a73b403fdc1c1b6971b611cc2db
Richard Romig <rick.romig@gmail.com>
2026-06-19 11:27:18 -0400

    fnloc-install.sh: updated and cleaned up

--- fnloc-install.sh
+++ fnloc-install.sh
@@ -1,32 +1,29 @@
-#!/bin/bash
-# fnloc-install.sh -- installs fnloc using source files
-# Copyright (C) 2018 Richard Romig
-# Email: rick.romig@gmail.com
-# Updated: 22 Nov 2025
+#!/usr/bin/env bash
+###############################################################################
+# Script Name  :  fnloc-install.sh
+# Description  : Installs fnloc using source files
+# Author       : Copyright (C) 2018 Richard Romig
+# Email        : rick.romig@gmail.com | rick.romig@mymetronet.com
+# Created      : 12 Apr 1998
+# Last updated : 19 Jun 2026
+# Comments     : Be sure the fnloc and lloc executable hinaries have been created,
+#              : either from the repository or from soucc before running the script.
+###############################################################################
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later version.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program. If not, see <https://www.gnu.org/licenses/>.
-#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+###############################################################################
 
 copy_executables() {
 	local -r repo_dir="$1"
 	local -r binary_dir="/usr/local/bin"
-	local -r script_dir="$HOME/bin"
 	[[ -d "$binary_dir" ]] || sudo mkdir -p "$binary_dir"
-	[[ -d "$script_dir" ]] || mkdir -p "$script_dir"
 	printf "Installing FnLoC...\n"
-	sudo cp -v "$repo_dir/fnloc" "$repo_dir/lloc" "$binary_dir/"
-	sudo cp -v "$repo_dir/*.sh" "$script_dir/"
+	sudo cp -v "$repo_dir/fnloc" "$repo_dir/lloc" "$repo_dir/loc2file.sh" "$binary_dir/"
 }
 
 copy_source_files() {
@@ -44,13 +41,16 @@ copy_docs() {
 	local -r docs_dir="/usr/local/doc/fnloc"
 	[[ -d "$docs_dir" ]] || sudo mkdir -p -v "$docs_dir"
 	printf "Copying program documentation...\n"
-	sudo cp -v "$repo_dir/changelog.md" "$repo_dir/README.md" "$docs_dir/"
-	sudo cp -v "$repo_dir/SHA256sum.tx" "$docs_dir/"
+	sudo cp -v "$repo_dir/*.md" "$docs_dir/"
 	sudo cp -v "$repo_dir/WARRANTY" "$repo_dir/LICENSE" "$docs_dir/"
 }
 
 main() {
-	local -r repo_dir="$HOME/Downloads/fnloc"
+	local -r repo_dir="$PWD"
+	if [[ ! -x fnloc || ! -x lloc ]]; then
+		printf "\e[91mERROR:\e[0m One or both binary files are missing.\n"
+		exit 1
+	fi
 	copy_executables "$repo_dir"
 	copy_source_files "$repo_dir"
 	copy_docs "$repo_dir"
